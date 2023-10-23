8e20e82e7c11f34d2da01e01218a7aa3eb2df6f9
Isaiah Nields <imnields@gmail.com>
2023-10-23 11:30:23 -0700

    add medium to footer

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -9,7 +9,7 @@ class Footer extends React.Component {
       <div>
         <Row>
           <Col>
-            <Social sites={['https://www.linkedin.com/in/isaiahnields/', 'https://twitter.com/isaiahnields', 'https://github.com/isaiahnields']} />
+            <Social sites={['https://www.linkedin.com/in/isaiahnields/', 'https://twitter.com/isaiahnields', 'https://github.com/isaiahnields', 'https://isaiahnields.medium.com']} />
           </Col>
           <Col>
             <Watermark />
