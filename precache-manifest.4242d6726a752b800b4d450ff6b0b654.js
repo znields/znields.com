79ff7cd7a43306df6bce8420a16362faa3f5afb2
Isaiah Nields <imnields@gmail.com>
2024-01-24 19:13:41 -0800

    Updates

---
+++ precache-manifest.4242d6726a752b800b4d450ff6b0b654.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e7617aca24efca911dd",
+    "url": "/static/css/main.147ac731.chunk.css"
+  },
+  {
+    "revision": "1e7617aca24efca911dd",
+    "url": "/static/js/main.78f67b65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a80c6eedb75ebbdec15f",
+    "url": "/static/js/2.d284add1.chunk.js"
+  },
+  {
+    "revision": "c329fe2130f995e095b9862b18ff0fab",
+    "url": "/static/media/galaxy.c329fe21.png"
+  },
+  {
+    "revision": "82f99acfebb8e2c150f5e69aa2644b81",
+    "url": "/static/media/data.82f99acf.png"
+  },
+  {
+    "revision": "218f9090ff39403813e53ba889ca0eb9",
+    "url": "/static/media/roast-bot-logo.218f9090.png"
+  },
+  {
+    "revision": "c331afd4341c4922688586cca41933d5",
+    "url": "/static/media/tree-of-simulations.c331afd4.png"
+  },
+  {
+    "revision": "f2442c277f807a16fabde1bff4a63357",
+    "url": "/index.html"
+  }
+];
--- src/store/day.js
+++
@@ -1,7 +0,0 @@
-import { observable } from 'mobx';
-
-const dayStore = observable({
-  day: (new Date()).getHours() < 19 && (new Date()).getHours() > 8,
-});
-
-export default dayStore;
