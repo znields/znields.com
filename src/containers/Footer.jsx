2701ae378350e540bd7f8661d62c00ade9466c30
Isaiah Nields <zaynields@gmail.com>
2024-07-15 21:17:26 -0700

    Update Footer.jsx

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Watermark from '../components/Watermark';
-import Social from '../components/Social';
-import { Row, Col } from 'react-bootstrap';
+import React from "react";
+import Watermark from "../components/Watermark";
+import Social from "../components/Social";
+import { Row, Col } from "react-bootstrap";
 
 class Footer extends React.Component {
   render() {
@@ -9,13 +9,20 @@ class Footer extends React.Component {
       <div>
         <Row>
           <Col>
-            <Social sites={['https://www.linkedin.com/in/znields/', 'https://x.com/z_nields', 'https://www.instagram.com/znields', 'https://github.com/znields', 'https://znields.medium.com']} />
+            <Social
+              sites={[
+                "https://www.linkedin.com/in/znields/",
+                "https://twitter.com/z_nields",
+                "https://www.instagram.com/znields",
+                "https://github.com/znields",
+                "https://znields.medium.com",
+              ]}
+            />
           </Col>
           <Col>
             <Watermark />
           </Col>
         </Row>
-
       </div>
     );
   }
